6c8a948218b11f5fe1aca201d65162636b59e2d7
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2024-03-24 17:19:18 -0500

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,12 @@
+# libraries
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np # linear algebra
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime as dt
+import plotly.express as px
+import plotly.figure_factory as ff
+import numpy as np
+
+df_2016 =pd.read_csv('properties_2016.csv')
+df_train_2016 =pd.read_csv('train_2016_v2.csv', parse_dates=["transactiondate"])
